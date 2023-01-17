cae6616876c99d320b47522b5a49e38b899395a2
PAS <pas@mail.ru>
2023-01-17 05:50:05 +0300

    Проверяем на наличие результата. Исключаем NULL

--- src/main/java/ru/pas/javalearn/StreamWithControl.java
+++ src/main/java/ru/pas/javalearn/StreamWithControl.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
 
-public class Stream {
+public class StreamWithControl {
 
     public static void main(String[] args) {
-        /* stream https://habr.com/ru/post/302628/
+        /** stream https://habr.com/ru/post/302628/
         https://metanit.com/java/tutorial/10.12.php
         Нахождение максимального значения исключая null значения */
         ArrayList<Integer> testValuesNull = new ArrayList<>();
@@ -17,8 +17,7 @@ public class Stream {
         testValuesNull.add(3, 70);
         testValuesNull.add(4, 50);
 
-        Optional<Integer> maxValueNotNull = testValuesNull.
-                stream().
+        Optional<Integer> maxValueNotNull = testValuesNull.stream().
                 // Исключаем Null записи
                 filter(Objects::nonNull).
                 // Ищем max
